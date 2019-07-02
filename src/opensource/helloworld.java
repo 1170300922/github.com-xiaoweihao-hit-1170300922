0acb7794cda827b2ed60182cb029b2dc3f5100ac
1170300922 <18800410616@163.com>
2019-07-02 19:06:30 +0900

    commit

---
+++ src/opensource/helloworld.java
@@ -0,0 +1,10 @@
+package opensource;
+import java.lang.*;
+public class helloworld {
+public static void hello() {
+  System.out.println("hello world");
+} 
+ public static void main(String[] args) {
+  hello();
+}
+}
