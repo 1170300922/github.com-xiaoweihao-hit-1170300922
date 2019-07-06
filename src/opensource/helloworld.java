aa4a471a69c6d70621864cae480b9dbb7ca6d66f
Mirakal <zehao323@gmail.com>
2019-07-06 11:52:24 +0800

    using for loop

--- src/opensource/helloworld.java
+++ src/opensource/helloworld.java
@@ -2,9 +2,9 @@ package opensource;
 import java.lang.*;
 public class helloworld {
 public static void hello() {
-  System.out.println("hello beautiful world!");
-  System.out.println("hello beautiful world!");
-  System.out.println("hello beautiful world!");
+	for (int i = 1; i <= 3; i++) {
+ 	 	System.out.println("hello beautiful world!");
+	}
 } 
  public static void main(String[] args) {
   hello();
